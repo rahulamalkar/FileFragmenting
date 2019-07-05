a3a77d6ae2204773d5c58e869a14a8bd2f4ea1bd
rahulamalkar <rrocky251@gmail.com>
2019-07-05 13:38:35 +0530

    added json structure for fragmenting file in code.cc.

--- src/code.cc
+++ src/code.cc
@@ -1,7 +1,9 @@
-#include <iostream>
-#include <fstream>
+#include "rapidjson/stringbuffer.h"
+#include "rapidjson/writer.h"
 #include "md5.h"
 #include "md5.cc"
+#include <iostream>
+#include <fstream>
 
 #define MAX_TRANSFER_SIZE	100
 
@@ -9,9 +11,25 @@ bool Fragmenter(std::string &contentFileName, std::string &content){
 		int contentLength = content.length();
 		std::string contentMd5 = md5(content);
 		std::cout << "md5: " << contentMd5 << std::endl;
-		int sequence = 0;
+		unsigned int sequence = 0;
+		unsigned int contentIndex = 0;
 		while((contentLength - MAX_TRANSFER_SIZE) > 0){
+				rapidjson::StringBuffer s;
+				rapidjson::Writer<rapidjson::StringBuffer> writer(s);
+
+				writer.StartObject();
+				writer.Key("key");
+				writer.String(contentMd5.c_str());
+				writer.Key("sequence");
+				writer.Uint(sequence);
+				writer.Key("data");
+				writer.String(content.substr (contentIndex, contentIndex + MAX_TRANSFER_SIZE).c_str());
+				writer.EndObject();
+				std::cout << s.GetString() << std::endl << std::endl;
+
 				contentLength -= MAX_TRANSFER_SIZE;
+				sequence++;
+				contentIndex += MAX_TRANSFER_SIZE;
 		}
 }
 
